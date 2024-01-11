1bec920436a8106ed09d2460cada6eaa4c9183a5
hixero <hystart109@gmail.com>
2024-01-12 00:47:13 +0900

    Initial Commit

---
+++ src/cats.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Header, Post, Param } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  @Header("Cache-Control", "none")
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
+  }
+}
